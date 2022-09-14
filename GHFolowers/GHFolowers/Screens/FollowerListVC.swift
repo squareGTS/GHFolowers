9e79360514e537112b41b0874ab04207fbba322e
Maxim Bekmetov <mbekmetov@ibs.ru>
2022-09-14 12:44:11 +0300

    quick bug fix

--- GHFolowers/GHFolowers/Screens/FollowerListVC.swift
+++ GHFolowers/GHFolowers/Screens/FollowerListVC.swift
@@ -15,7 +15,13 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
 }
