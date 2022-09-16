d98f7bc41160f9bbd2dcb6c105d0bcd8d8859c49
Maxim Bekmetov <mbekmetov@ibs.ru>
2022-09-16 17:43:21 +0300

    user info screen

--- GHFolowers/GHFolowers/Managers/NetworkManager.swift
+++ GHFolowers/GHFolowers/Managers/NetworkManager.swift
@@ -23,7 +23,7 @@ class NetworkManager {
             return
         }
 
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        URLSession.shared.dataTask(with: url) { data, response, error in
 
             if let _ = error {
                 completed(.failure(.unableToComplete))
@@ -48,8 +48,42 @@ class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }.resume()
+    }
+
+    func getUserInfo(for username: String, completed: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + "\(username)"
+
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidUserName))
+            return
         }
 
-        task.resume()
+        URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }.resume()
     }
 }
---
+++ GHFolowers/GHFolowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFolowers
+//
+//  Created by Maxim Bekmetov on 16.09.2022.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
