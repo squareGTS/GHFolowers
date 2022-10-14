0650e1c04dd8f997e56ab344d55d4aa77a40ff0a
Maxim Bekmetov <mbekmetov@ibs.ru>
2022-10-14 12:46:14 +0500

    add delegate patern

--- GHFolowers/GHFolowers/Custom views/ViewControllers/ItemInfoVC/GFItemInfoVC.swift
+++ GHFolowers/GHFolowers/Custom views/ViewControllers/ItemInfoVC/GFItemInfoVC.swift
@@ -15,7 +15,7 @@ class GFItemInfoVC: UIViewController {
     let actionButton    = GFButton()
 
     var user: User!
-    var delegate: UserInfoVCDelegate!
+    weak var delegate: UserInfoVCDelegate!
 
     init(user: User) {
         super.init(nibName: nil, bundle: nil)
@@ -29,6 +29,7 @@ class GFItemInfoVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
+        configureActionButton()
         layoutUI()
         configureStackView()
     }
@@ -44,6 +45,14 @@ class GFItemInfoVC: UIViewController {
 
         stackView.addArrangedSubview(itemInfoViewOne)
         stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+
+    private func configureActionButton() {
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func actionButtonTapped() {
+
     }
 
     private func layoutUI() {
